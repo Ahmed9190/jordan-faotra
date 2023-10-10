915ae5b5da9b52b83f160f7417475eee3e948f5f
Ahmed Waleed <ahmed.waleed919@outlook.com>
2023-10-10 22:59:13 +0300

    checked if xml is already set

--- src/models/invoice.ts
+++ src/models/invoice.ts
@@ -23,28 +23,29 @@ export default class Invoice {
   }
 
   toXmlString(): string {
-    this.xmlString = this.invoiceBuilder
-      .create({
-        id: this.invoiceData.id,
-        uuid: this.invoiceData.uuid,
-        invoiceNumber: this.invoiceData.invoiceNumber,
-        issueDate: this.invoiceData.issueDate,
-        invoiceType: this.invoiceData.invoiceType,
-        note: this.invoiceData.note,
-        currencyCode: this.invoiceData.currencyCode,
-        supplier: this.invoiceData.supplier,
-        customer: this.invoiceData.customer,
-        invoiceLines: this.invoiceData.invoiceLines,
-        incomeSourceSequence: this.invoiceData.incomeSourceSequence,
-        countryCode: this.invoiceData.countryCode,
-        totalDiscount: this.invoiceData.totalDiscount,
-        taxAmount: this.invoiceData.taxAmount,
-        taxExclusiveAmount: this.invoiceData.taxExclusiveAmount,
-        taxInclusiveAmount: this.invoiceData.taxInclusiveAmount,
-        allowanceTotalAmount: this.invoiceData.allowanceTotalAmount,
-        payableAmount: this.invoiceData.payableAmount,
-      })
-      .build();
+    if (!this.xmlString)
+      this.xmlString = this.invoiceBuilder
+        .create({
+          id: this.invoiceData.id,
+          uuid: this.invoiceData.uuid,
+          invoiceNumber: this.invoiceData.invoiceNumber,
+          issueDate: this.invoiceData.issueDate,
+          invoiceType: this.invoiceData.invoiceType,
+          note: this.invoiceData.note,
+          currencyCode: this.invoiceData.currencyCode,
+          supplier: this.invoiceData.supplier,
+          customer: this.invoiceData.customer,
+          invoiceLines: this.invoiceData.invoiceLines,
+          incomeSourceSequence: this.invoiceData.incomeSourceSequence,
+          countryCode: this.invoiceData.countryCode,
+          totalDiscount: this.invoiceData.totalDiscount,
+          taxAmount: this.invoiceData.taxAmount,
+          taxExclusiveAmount: this.invoiceData.taxExclusiveAmount,
+          taxInclusiveAmount: this.invoiceData.taxInclusiveAmount,
+          allowanceTotalAmount: this.invoiceData.allowanceTotalAmount,
+          payableAmount: this.invoiceData.payableAmount,
+        })
+        .build();
 
     return this.xmlString!;
   }
