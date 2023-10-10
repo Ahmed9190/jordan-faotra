9a9f0606ae45ef463eefd14e96286e3673ff25ff
Ahmed Waleed <ahmed.waleed919@outlook.com>
2023-10-10 23:09:50 +0300

    created another method for direct and returns only built xml

--- src/index.ts
+++ src/index.ts
@@ -100,6 +100,43 @@ app.post(
       }
     );
 
+    const xmlString = invoice.toXmlString();
+
+    res.set("Content-Type", "application/xml");
+    res.send(xmlString);
+  }
+);
+
+app.post(
+  "/invoice/direct",
+  (req: Request<never, never, CreateInvoiceDto>, res: Response) => {
+    const { customer, ...invoiceData } = req.body;
+    const invoice: Invoice = new Invoice(
+      {
+        uuid: uuidv4(),
+        issueDate: new Date(),
+        invoiceType: InvoiceType.createNewCashInvoice,
+        currencyCode: "JOD",
+        supplier: {
+          countryCode: "JO",
+          taxNumber: 11803860,
+          name: "شركة الافق لصناعة الزجاج",
+        },
+        customer: {
+          countryCode: "JO",
+          ...customer,
+        },
+        countryCode: "JO",
+        incomeSourceSequence: 12758574,
+        ...invoiceData,
+      },
+      {
+        "Client-Id": process.env.CLIENT_ID!,
+        "Secret-Key": process.env.SECRET_KEY!,
+        Cookie: process.env.COOKIE!,
+      }
+    );
+
     const headers = invoice.getFaotraRequestHeaders();
     const jsonBody = invoice.toJson();
 
@@ -113,83 +150,21 @@ app.post(
       )
       .then(({ data }) => {
         res.send({
-          json: req.body,
-          request: invoice.toXmlString(),
+          request: jsonBody,
           response: data,
         });
       })
       .catch((error) => {
         if (isAxiosError(error)) {
           const data = error.response?.data;
-          const errorsResponse = formatErrors(data);
-
           res.send({
-            json: req.body,
-            request: invoice.toXmlString(),
+            request: jsonBody,
             response: data,
-            ...errorsResponse,
           });
         }
       });
   }
 );
-
-// const invoice: Invoice = new Invoice(
-//   {
-//     id: "EIN00090",
-//     uuid: "0043e15e-740b-4e1b-889d-8504afdb1d1d",
-//     invoiceNumber: 1,
-//     issueDate: new Date(Date.parse("2022-09-27")),
-//     invoiceType: InvoiceType.createNewCashInvoice,
-//     note: "ملاحظات 22",
-//     currencyCode: "JOD",
-//     supplier: { countryCode: "JO", taxNumber: 11803860, name: "شركة الافق لصناعة الزجاج" },
-//     customer: {
-//       name: "امجد سليمان",
-//       customerPartyNumber: 33445544,
-//       postalCode: 33554,
-//       countryCode: "JO",
-//       countrySubentityCode: "AZ",
-//       telephone: "324323434",
-//     },
-//     invoiceLines: [
-//       {
-//         itemId: String(1),
-//         currencyId: "JO",
-//         quantity: 33.0,
-//         unitPrice: 2.0,
-//         nameOrDescription: "زهره",
-//         discount: 2,
-//         countryCode: "JO",
-//         taxAmount: 4.48,
-//         roundingAmount: 68.48,
-//         generalTaxPercent: 7,
-//       },
-//     ],
-//     incomeSourceSequence: 12758574,
-//     countryCode: "JO",
-//     totalDiscount: 2,
-//     taxAmount: 4.48,
-//     taxExclusiveAmount: 66,
-//     taxInclusiveAmount: 68.48,
-//     allowanceTotalAmount: 2,
-//     payableAmount: 68.48,
-//   },
-//   {
-//     "Client-Id": process.env.CLIENT_ID!,
-//     "Secret-Key": process.env.SECRET_KEY!,
-//     Cookie: process.env.COOKIE!,
-//   }
-// );
-
-// const headers = invoice.getFaotraRequestHeaders();
-// const jsonBody = invoice.toJson();
-
-// axios
-//   .post("https://backend.jofotara.gov.jo/core/invoices/", jsonBody, {
-//     headers: { ...headers },
-//   })
-//   .then(({ data }) => fs.writeFileSync("response.json", JSON.stringify(data)));
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
